20734278e86cbe139cc77df30085162abe263666
Rafa <rcj.es1997@gmail.com>
2023-02-27 20:29:27 +0100

    Clase 29 - Zuul filtro

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
-//import org.springframework.core.env.Environment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +17,8 @@ import com.formacionbdi.springboot.app.productos.models.service.IProductoService
 @RestController
 public class ProductoController {
 	
-	//@Autowired
-	//private Environment env;
+	@Autowired
+	private Environment env;
 	
 	@Value("${server.port}")
 	private Integer port;
@@ -32,8 +32,8 @@ public class ProductoController {
 	@GetMapping("/listar")
 	public List<Producto> listar(){
 		return productoService.findAll().stream().map(producto ->{
-			//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-			producto.setPort(port);
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+//			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -41,9 +41,9 @@ public class ProductoController {
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
 		Producto producto = productoService.findById(id);
-		//producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		//producto.setPort(port);
-		producto.setPort(webServerAppCtxt.getWebServer().getPort());
+//		producto.setPort(webServerAppCtxt.getWebServer().getPort());
 		
 //		try {
 //			Thread.sleep(2000L);
