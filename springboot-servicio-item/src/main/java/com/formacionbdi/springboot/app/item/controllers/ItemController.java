8c6bdea4b5d3ee1084723de1885d6787ee099c1b
Rafa <rcj.es1997@gmail.com>
2023-03-01 20:13:16 +0100

    Clase 46

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -2,8 +2,11 @@ package com.formacionbdi.springboot.app.item.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.formacionbdi.springboot.app.item.models.Item;
 import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 
 @RestController
 public class ItemController {
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
+	
+	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 	
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -30,14 +38,17 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+//	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return itemService.findById(id, cantidad);
+		return cbFactory.create("items")
+				.run(() -> itemService.findById(id, cantidad)/*, e -> metodoAlternativo(id, cantidad, e)*/);
 	}
 	
 	
-	public Item metodoAlternativo(Long id, Integer cantidad) {
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		
 		Item item = new Item();
 		item.setCantidad(cantidad);
 		
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,11 +1,12 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,16 +36,19 @@ public class ProductoController {
 	}
 
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
+	public Producto detalle(@PathVariable Long id) throws InterruptedException {
+		
+		if (id.equals(10L)) {
+			throw new IllegalStateException("Producto no encontrado");
+		}
+		
+		if (id.equals(7L)) {
+			TimeUnit.SECONDS.sleep(5);
+		}
+		
 		Producto producto = productoService.findById(id);
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 //		producto.setPort(port);
-
-//		try {
-//			Thread.sleep(2000L);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
 
 		return producto;
 	}
