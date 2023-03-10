052342ebcdb6194df6d4189d09d4cc266ac4f265
Rafa <rcj.es1997@gmail.com>
2023-03-10 18:21:58 +0100

    Arreglo maven

--- springboot-servicio-commons/src/test/java/com/formacionbdi/springboot/app/commons/SpringbootServicioConfigCommonsApplicationTests.java
+++ springboot-servicio-commons/src/test/java/com/formacionbdi/springboot/app/commons/SpringbootServicioConfigCommonsApplicationTests.java
@@ -1,9 +1,14 @@
 package com.formacionbdi.springboot.app.commons;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import com.formacionbdi.app.commons.SpringbootServicioConfigCommonsApplication;
+
+@SpringBootTest(classes = {SpringbootServicioConfigCommonsApplication.class})
+@RunWith(SpringRunner.class)
 class SpringbootServicioConfigCommonsApplicationTests {
 
 	@Test
