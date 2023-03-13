4a1dfd7934d573b1050d44691ccee34bc7550c48
Rafa <rcj.es1997@gmail.com>
2023-03-13 19:38:59 +0100

    Clase 89

--- springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/commons/usuarios/models/entity/Usuario.java
+++ springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/commons/usuarios/models/entity/Usuario.java
@@ -1,4 +1,4 @@
-package com.formacionbdi.springboot.app.usuarios.models.entity;
+package com.formacionbdi.springboot.app.commons.usuarios.models.entity;
 
 import java.io.Serializable;
 import java.util.List;
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
@@ -5,8 +5,8 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Role;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
