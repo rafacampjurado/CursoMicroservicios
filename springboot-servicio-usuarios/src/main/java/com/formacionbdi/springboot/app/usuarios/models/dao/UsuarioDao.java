0675ab6491332251706eccb89cd284415937948c
Rafa <rcj.es1997@gmail.com>
2023-03-13 18:49:04 +0100

    Clase 85

---
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,18 @@
+package com.formacionbdi.springboot.app.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+
+@RepositoryRestResource(path = "usuarios")
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
+	
+	public Usuario findByUsername(String username);
+	
+	public Usuario findByUsernameAndEmail(String username, String email);
+	
+	@Query("select u from Usuario u where u.username=?1")
+	public Usuario obtenerPorUsername(String username);
+}
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/entity/Usuario.java
@@ -1,13 +1,19 @@
 package com.formacionbdi.springboot.app.usuarios.models.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "usuarios")
@@ -27,8 +33,25 @@ public class Usuario implements Serializable {
 	private Boolean enabled;
 	private String nombre;
 
-	@Column(unique = true, length = 100)
+	@Column(length = 100)
 	private String apellido;
+	
+	@Column(unique = true, length = 100)
+	private String email;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuarios_to_roles", joinColumns = @JoinColumn(name="usuario_id"), 
+	inverseJoinColumns = @JoinColumn(name="role_id"),
+	uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "role_id"})})
+	private List<Role> roles;
+	
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 
 	public Long getId() {
 		return id;
@@ -77,5 +100,14 @@ public class Usuario implements Serializable {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 
 }
